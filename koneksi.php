40cb50dab93cbf2e854b43045e89b31d8486bf57
acengberak <adit.goyo@gmail.com>
2019-12-11 18:02:58 +0700

    FORM UPDATE DONE

--- koneksi.php
+++ koneksi.php
@@ -20,7 +20,7 @@ class Database{
 
         function create($id,$foto,$nama,$alamat,$umur,$gaji,$kelamin,$jabatan,$anak)
         {
-            $sql = "INSERT INTO karyawan (id_karyawan, foto_karyawan, Nama, alamat, umur, gaji, jenis_kelamin, jabatan, jumlah_anak) VALUES ('$id', '$foto', '$nama', '$alamat', '$umur', '$gaji', '$kelamin', '$jabatan', '$anak')";
+            $sql = "INSERT INTO karyawan (id_karyawan, foto_karyawan, Nama, alamat, umur, gaji, jenis_kelamin, jabatan, Jumlah_anak) VALUES ('$id', '$foto', '$nama', '$alamat', '$umur', '$gaji', '$kelamin', '$jabatan', '$anak')";
 
             $res = mysqli_query($this->connection,$sql);
             if($res){
@@ -32,12 +32,34 @@ class Database{
 
         function read ($id = null){
             $sql = "SELECT * FROM karyawan";
-            if($id){ $sql .= "WHERE id_karyawan='$id'";}
+            if($id){ $sql .= "WHERE id_karyawan=$id";}
             $res = mysqli_query($this->connection,$sql);
             return $res;
         }
 
-  
+
+        function update($foto,$nama,$alamat,$umur,$gaji,$kelamin,$jabatan,$anak,$id)
+        {
+            $sql = "UPDATE karyawan SET foto_karyawan='$foto', Nama='$nama', alamat='$alamat', umur='$umur', gaji='$gaji', jenis_kelamin='$kelamin', jabatan='$jabatan', Jumlah_anak='$anak' WHERE id_karyawan=$id";
+            $res = mysqli_query($this->connection,$sql);
+            if($res){
+                return True;
+            }else{
+                return false;
+            }
+        }
+
+          function delete($id)
+    {
+        $sql = "DELETE FROM karyawan WHERE id_karyawan=$id";
+        $res = mysqli_query($this->connection, $sql);
+        if ($res) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 
         
--- show.php
+++ show.php
@@ -22,7 +22,7 @@
 <body>
 
 
-    <div class="table container-fluid text-center">
+    <div class="table container-fluid ">
         <div class="card">
             <div class="card-body">
                 <table class="table">
@@ -49,7 +49,6 @@
                         $sql = $database->read();
                         $no = 1;
                         while ($data = mysqli_fetch_assoc($sql)) {
-
                             ?>
                             <tr>
                                 <td><?= $no++ ?> </td>
@@ -64,12 +63,114 @@
                                 <td><?php echo $data['Jumlah_anak'] ?> Anak</td>
                                 <!-- Edit -->
                                 <td>
-                                    <a id="edit_info" data-toggle="modal" data-target="#updateModal" data-id=>
-                                    <a href="#updatemodal" class="btn btn-primary" data-toggle="modal" data-target="#updateModal<?= $data['id_karyawan'];?>">Edit</a>
+
+                                    <a href="#updatemodal" class="btn btn-primary" data-toggle="modal" data-target="#updateModal<?= $data['id_karyawan']; ?>">Edit</a>
                                     <!-- delete -->
-                                    <a href="delete.php?id=<?php echo $data['id_karyawan']; ?>" class="btn btn-danger">Delete</a>
+                                    <a href="#delete" data-toggle="modal" data-target="#delete" class="btn btn-danger">Delete</a>
                                 </td>
                             </tr>
+                            <!-- MODAL FORM -->
+                            <div class="modal fade" id="updateModal<?= $data['id_karyawan']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 style="color: #0065b3;" class="modal-title" id="exampleModalLabel">Garuda Indonesia</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+
+                                        <!-- FORM -->
+                                        <form action="show.php" method="POST">
+                                            <div class="modal-body">
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        <input type="hidden" name="id_karyawan" value="<?= $data['id_karyawan'] ?>">
+                                                        <!-- FOTO KARYAWAB -->
+                                                        <div class="form-group">
+                                                            <label for="foto"><b>Upload Foto</b></label>
+                                                            <input type="file" name="foto_karyawan" class="form-control-file" id="foto">
+                                                        </div>
+                                                        <!-- NAMA -->
+                                                        <div class="form-group">
+                                                            <label for="input"><b>Nama</b></label>
+                                                            <input type="text" name="Nama" class="form-control" id="nama" value="<?php echo $data['Nama']; ?>">
+                                                        </div>
+                                                        <!-- ALAMAT -->
+                                                        <div class="form-group">
+                                                            <label for="input"><b>Alamat</b></label>
+                                                            <input type="text" name="alamat" class="form-control" id="alamat" value="<?php echo $data['alamat']; ?>">
+                                                        </div>
+
+                                                        <div class="form-group">
+                                                            <div class="row">
+                                                                <div class="col-4">
+                                                                    <!-- UMUR -->
+                                                                    <label for="input"><b>Umur</b></label>
+                                                                    <input type="text" name="umur" class="form-control" id="umur" value="<?php echo $data['umur']; ?>">
+                                                                </div>
+                                                                <div class="col-7">
+                                                                    <!-- GAJI -->
+                                                                    <label for="input"><b>Gaji</b></label>
+                                                                    <input type="text" name="gaji" class="form-control" id="gaji" value="<?php echo $data['gaji']; ?>">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group" class="radio">
+                                                            <!-- Jenis Kelamin -->
+                                                            <label for="input1"><b>Jenis Kelamin</b></label><br>
+                                                            <input type="radio" name="jenis_kelamin" id="optionsRadio1" value="pria" <?php if ($data['jenis_kelamin'] == 'pria') {
+                                                                                                                                                echo "checked";
+                                                                                                                                            } ?>>Pria &nbsp;
+                                                            <input type="radio" name="jenis_kelamin" id="optionsRadio1" value="wanita" <?php if ($data['jenis_kelamin'] == 'wanita') {
+                                                                                                                                                echo "checked";
+                                                                                                                                            } ?>>wanita
+                                                        </div>
+
+                                                        <div class="form-group">
+                                                            <!-- Jabatan -->
+                                                            <label for="input"><b>Jabatan</b></label>
+                                                            <input type="text" name="jabatan" class="form-control" id="jabatan" value="<?php echo $data['jabatan']; ?>">
+                                                        </div>
+
+                                                        <!-- Jumlah anak -->
+                                                        <div class="form-group">
+                                                            <label for="input1"><b>Jumlah Anak</b></label>
+                                                            <select name="Jumlah_anak" class="form-control">
+                                                                <option>Jumlah Anak</option>
+                                                                <option value="0" <?php if ($data['Jumlah_anak'] == '0') {
+                                                                                            echo "selected";
+                                                                                        } ?>>Belum Ada</option>
+                                                                <option value="1" <?php if ($data['Jumlah_anak'] == '2') {
+                                                                                            echo "selected";
+                                                                                        } ?>>1</option>
+                                                                <option value="2" <?php if ($data['Jumlah_anak'] == '2') {
+                                                                                            echo "selected";
+                                                                                        } ?>>2</option>
+                                                                <option value="3" <?php if ($data['Jumlah_anak'] == '3') {
+                                                                                            echo "selected";
+                                                                                        } ?>>3</option>
+                                                                <option value="4" <?php if ($data['Jumlah_anak'] == '4') {
+                                                                                            echo "selected";
+                                                                                        } ?>>4</option>
+                                                                <option value="5" <?php if ($data['Jumlah_anak'] == '5') {
+                                                                                            echo "selected";
+                                                                                        } ?>>5</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <!-- BUTTON DALAM MODAL -->
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <button type="submit" name="simpan" id="updateModal" class="btn btn-primary" value="simpan">Save</button>
+                                        </form>
+                                        <!-- END Form -->
+                                    </div>
+                                </div>
+                            </div>
                         <?php } ?>
                     </tbody>
                 </table>
@@ -77,103 +178,49 @@
         </div>
     </div>
 
-    <?php
-    require_once('koneksi.php');
-
-    $sql = $database->read();
-    while ($data = mysqli_fetch_assoc($sql)) { ?>
-        <!-- MODAL -->
-        <div class="modal fade" id="updateModal<?= $data['id_karyawan'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 style="color: #0065b3;" class="modal-title" id="exampleModalLabel">Garuda Indonesia</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-
-                    <!-- FORM -->
-                    <form action="update.php" method="GET">
-                        <div class="modal-body">
-                            <div class="row justify-content-center">
-                                <div class="col-12">
-                                    <input type="hidden" name="id_karyawan">
-                                    <!-- FOTO KARYAWAB -->
-                                    <div class="form-group">
-                                        <label for="foto"><b>Upload Foto</b></label>
-                                        <input type="file" name="foto_karyawan" class="form-control-file" id="foto">
-                                    </div>
-                                    <!-- NAMA -->
-                                    <div class="form-group">
-                                        <label for="input"><b>Nama</b></label>
-                                        <input type="text" name="Nama" class="form-control" id="nama" value="<?php echo $data['Nama']; ?>">
-                                    </div>
-                                    <!-- ALAMAT -->
-                                    <div class="form-group">
-                                        <label for="input"><b>Alamat</b></label>
-                                        <input type="text" name="alamat" class="form-control" id="alamat" value="<?php echo $data['alamat']; ?>">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-4">
-                                                <!-- UMUR -->
-                                                <label for="input"><b>Umur</b></label>
-                                                <input type="text" name="umur" class="form-control" id="umur" placeholder="umur">
-                                            </div>
-                                            <div class="col-7">
-                                                <!-- GAJI -->
-                                                <label for="input"><b>Gaji</b></label>
-                                                <input type="text" name="gaji" class="form-control" id="gaji" placeholder="1000000">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group" class="radio">
-                                        <!-- Jenis Kelamin -->
-                                        <label for="input1"><b>Jenis Kelamin</b></label><br>
-                                        <input type="radio" name="gender" id="optionsRadio1" value="pria" checked>Pria &nbsp;
-                                        <input type="radio" name="gender" id="optionsRadio1" value="wanita" checked>wanita
-                                    </div>
-
-                                    <div class="form-group">
-                                        <!-- Jabatan -->
-                                        <label for="input"><b>Jabatan</b></label>
-                                        <input type="text" name="jabatan" class="form-control" id="jabatan" placeholder="Jabatan">
-                                    </div>
-
-                                    <!-- Jumlah anak -->
-                                    <div class="form-group">
-                                        <label for="input1"><b>Jumlah Anak</b></label>
-                                        <select name="anak" class="form-control">
-                                            <option>Jumlah Anak</option>
-                                            <option value="0">Belum Ada</option>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                        </select>
-                                    </div>
-
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <!-- BUTTON DALAM MODAL -->
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" name="simpan" id="updatedata" class="btn btn-primary" value="simpan">Save</button>
-                    </form>
-                    <!-- END Form -->
+    <div class="modal" id="delete" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Apakah Anda Yakin?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Ya</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
-    <?php } ?>
+    </div>
+
+    <?php
+    require_once('koneksi.php');
+    if (isset($_POST) & !empty($_POST)) {
+        $id = $_POST['id_karyawan'];
+        $foto = $_POST['foto_karyawan'];
+        $nama = $_POST['Nama'];
+        $alamat = $_POST['alamat'];
+        $umur = $_POST['umur'];
+        $gaji = $_POST['gaji'];
+        $kelamin = $_POST['jenis_kelamin'];
+        $jabatan = $_POST['jabatan'];
+        $anak = $_POST['Jumlah_anak'];
+        $res = $database->update($foto, $nama, $alamat, $umur, $gaji, $kelamin, $jabatan, $anak, $id);
+        if ($res) {
+            echo '<script> alert("DATA UPDATE"); </script>';
+        } else {
+            echo '<script> alert("DATA GAGAL");</script>';
+        }
+    }
 
 
 
+    ?>
 
 
 
